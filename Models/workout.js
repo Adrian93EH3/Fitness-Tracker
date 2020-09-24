03598777a978e073ee260c2914ecc0df7862f377
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-23 23:17:47 -0400

    deploying

--- Models/workout.js
+++ Models/workout.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    
     day: {
         type: Date,
         // creates date if no date
