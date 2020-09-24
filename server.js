863c637b1ebd327f34c798e366fc26319892fdea
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-23 20:38:27 -0400

    adding server.js to have a local host and starting MVC structure

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}
+);
+
+require("./controllers/api-routes")(app);
+require("./controllers/html-routes")(app);
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT:" + PORT)
+});
